1ca7959417529bcf2083adbc4359ae5f8e1d58d8
Felix Mueller <felix.mueller@camunda.com>
2018-05-02 14:17:08 +0200

    Step-2b

---
+++ nodejs/charge-card-worker/worker.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Client, logger } from 'camunda-external-task-client-js';
+import open from 'open';
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+//  - 'asyncResponseTimeout': long polling timeout (then a new request will be issued)
+const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger, asyncResponseTimeout: 10000 };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'charge-card'
+client.subscribe('charge-card', async function({ task, taskService }) {
+  // Put your business logic here
+
+  // Get a process variable
+  const amount = task.variables.get('amount');
+  const item = task.variables.get('item');
+
+  console.log(`Charging credit card with an amount of ${amount}€ for the item '${item}'...`);
+  
+  open('https://docs.camunda.org/get-started/quick-start/success');
+  
+  // Complete the task
+  await taskService.complete(task);
+});
